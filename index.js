42b28c5c111965ac7b302fa02768b886d7fb75d3
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-31 09:35:20 +0000

    ggg

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+const friends = [
+  { id: 1, name: 'Shaun' },
+  { id: 2, name: 'Megan' },
+  { id: 3, name: 'Pere' },
+];
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/friends', (req, res, next) => {
+  res.json(friends);
+});
+
+app.listen(port, () => {
+  console.log('listening on ' + port);
+});
