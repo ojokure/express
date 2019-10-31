8cb61ce711f7f65bd54b2241944b77ea8a2b7753
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-31 10:15:03 +0000

    ggg

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 const friends = [
-  { id: 1, name: 'Shaun' },
-  { id: 2, name: 'Megan' },
-  { id: 3, name: 'Pere' },
+  { id: 1, name: "Shaun" },
+  { id: 2, name: "Megan" },
+  { id: 3, name: "Pere" }
 ];
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/friends', (req, res, next) => {
+app.get("/api/friends", (req, res, next) => {
   res.json(friends);
 });
 
 app.listen(port, () => {
-  console.log('listening on ' + port);
+  console.log("listening on " + port);
 });
